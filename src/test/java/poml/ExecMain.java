8067864bda0a3768d04d5a0697344033e5b99fd9
mamorum <mamoru310@yahoo.com>
2016-09-19 15:27:56 +0900

    Fix file name.

--- src/test/java/poml/ExecMain.java
+++ src/test/java/poml/ExecMain.java
@@ -6,7 +6,7 @@ public class ExecMain {
 
   public static void main(String[] args) throws IOException {
     Main.main(
-      new String[]{"pom.pmol", "pom.xml"}
+      new String[]{"pom.poml", "pom.xml"}
     );
   }
 
