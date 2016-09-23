97cd2ec275dcc7ab4386da9ba063c2e59af083db
mamorum <mamoru310@yahoo.com>
2016-09-23 11:53:27 +0900

    Polish.

--- src/main/java/poml/converter/basic/Lib.java
+++ src/main/java/poml/converter/basic/Lib.java
@@ -1,43 +1,38 @@
 package poml.converter.basic;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import poml.Converter;
 import poml.Dst;
 import poml.Src;
 
 public class Lib extends Converter {
 
-  public String name() { return "lib"; }
+  @Override public String name() { return "lib"; }
   
-  private static final String[] tmpls = {
-      "      <groupId>0</groupId>", 
-      "      <artifactId>1</artifactId>",
-      "      <version>2</version>",
-      "      <scope>3</scope>",
-      "      <optional>4</optional>",
-      "      <type>5</type>"
+  private static final String[] tags = {
+    "groupId", "artifactId", "version",
+    "scope", "optional", "type"
   };
 
   @Override public void convert(Src src, Dst dst) {
-    dst.out.println("  <dependencies>");
+    dst.out.println(sp2 + "<dependencies>");
     for (String lib: src.propList(name(), ",")) {
-      addLib(lib.trim(), dst);
+      printDependncy(lib.trim(), dst);
     }
-    dst.out.println("  </dependencies>");
+    dst.out.println(sp2 + "</dependencies>");
   }
 
-  public void addLib(String lib, Dst dst) {
-    dst.out.println("    <dependency>");
+  public void printDependncy(String lib, Dst dst) {
+    dst.out.println(sp4 + "<dependency>");
+    Map<String, String> kv = new LinkedHashMap<>();
     String[] vals = lib.split(":");
+    // TODO check vals (null, length)
     for (int i = 0; i < vals.length; i++) {
-      dst.out.println(tag(i, vals[i]));
+      kv.put(tags[i], vals[i]);
     }
-    dst.out.println("    </dependency>");
-  }
-
-  public String tag(int index, String val) {
-    return tmpls[index].replace(
-        Integer.toString(index),
-        val.trim()
-    );
+    dst.printKvTags(sp6, kv);
+    dst.out.println(sp4 + "</dependency>");
   }
 }
--- src/main/java/poml/converter/project/Model4.java
+++ src/main/java/poml/converter/project/Model4.java
@@ -5,26 +5,22 @@ import poml.Dst;
 import poml.Src;
 
 public class Model4 {
-  
+
   public static class Start extends Converter {
-    public String name() { return "#model4"; }
-    private static final String model4 =  // for maven2.
-      "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" " +
+    @Override public String name() { return "#model4"; }
+    @Override public void convert(Src src, Dst dst) {
+      dst.out.println(
+        "<project xmlns=\"http://maven.apache.org/POM/4.0.0\" " +
         "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
         "xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 " +
-        "http://maven.apache.org/xsd/maven-4.0.0.xsd\">" +
-        nl +
-      "  <modelVersion>4.0.0</modelVersion>";
-    @Override public void convert(Src src, Dst dst) {
-      dst.out.println(model4); 
+        "http://maven.apache.org/xsd/maven-4.0.0.xsd\">"); 
+      dst.out.println(sp2 + "<modelVersion>4.0.0</modelVersion>");
     }
   }
-  
   public static class End extends Converter {
-    public String name() { return "/model4"; }
-    private static final String tag = "</project>";
+    @Override public String name() { return "/model4"; }
     @Override public void convert(Src src, Dst dst) {
-      dst.out.println(tag);
+      dst.out.println("</project>");
     }
   }
 }
