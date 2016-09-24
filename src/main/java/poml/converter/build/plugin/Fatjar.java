9b671ea6061323c3133c251e3f123453750fe1a8
mamorum <mamoru310@yahoo.com>
2016-09-24 16:19:38 +0900

    Update fatjar.

--- src/main/java/poml/converter/build/plugin/Fatjar.java
+++ src/main/java/poml/converter/build/plugin/Fatjar.java
@@ -15,7 +15,10 @@ public class Fatjar implements Converter {
 
   @Override public void convert(Src src, Dst dst) {
     Map<String, String> map = src.propMap(name());
-    Assert.exists("mainClass", map, name());
+    Assert.exists(
+        new String[] {"mainClass", "jarName"},
+        map, name()
+    );
     Put.defaults("ver", "2.6", map);
     Tmpl.render("/tmpl/fatjar.tmpl", map, dst);
   }
