26b9944f3d69d5ce6023dfcc4e03e20c2e72d060
mamorum <mamoru310@yahoo.com>
2016-09-25 13:35:56 +0900

    Add fatjar test.

---
+++ src/test/java/poml/converter/build/plugin/FatjarTest.java
@@ -0,0 +1,79 @@
+package poml.converter.build.plugin;
+
+import org.junit.Test;
+
+import poml.ConverterCase;
+
+public class FatjarTest extends ConverterCase {
+
+  Fatjar conveter = new Fatjar();
+
+  @Test public void defaultVer() {
+    src.prop.put("fatjar",
+      "  jarName:${project.artifactId}," + 
+      "  mainClass:org.sample.Main");
+    conveter.convert(src, dst);
+    output.is(
+        "      <plugin>" + nl + 
+        "        <groupId>org.apache.maven.plugins</groupId>" + nl +
+        "        <artifactId>maven-assembly-plugin</artifactId>" + nl +
+        "        <version>2.6</version>" + nl +
+        "        <configuration>" + nl +
+        "          <finalName>${project.artifactId}</finalName>" + nl +
+        "          <descriptorRefs>" + nl +
+        "            <descriptorRef>jar-with-dependencies</descriptorRef>" + nl +
+        "          </descriptorRefs>" + nl +
+        "          <appendAssemblyId>false</appendAssemblyId>" + nl +
+        "          <attach>false</attach>" + nl +
+        "          <archive>" + nl +
+        "            <manifest>" + nl +
+        "              <mainClass>org.sample.Main</mainClass>" + nl +
+        "            </manifest>" + nl +
+        "          </archive>" + nl +
+        "        </configuration>" + nl +
+        "        <executions>" + nl +
+        "          <execution>" + nl +
+        "            <id>make-assembly</id>" + nl +
+        "            <phase>package</phase>" + nl +
+        "            <goals><goal>single</goal></goals>" + nl +
+        "          </execution>" + nl +
+        "        </executions>" + nl +
+        "      </plugin>" + nl
+      );
+  }
+  
+  @Test public void ver() {
+    src.prop.put("fatjar",
+        "  ver:1.0.0," +
+        "  jarName:${project.artifactId}," + 
+        "  mainClass:org.sample.Main");
+    conveter.convert(src, dst);
+    output.is(
+        "      <plugin>" + nl + 
+        "        <groupId>org.apache.maven.plugins</groupId>" + nl +
+        "        <artifactId>maven-assembly-plugin</artifactId>" + nl +
+        "        <version>1.0.0</version>" + nl +
+        "        <configuration>" + nl +
+        "          <finalName>${project.artifactId}</finalName>" + nl +
+        "          <descriptorRefs>" + nl +
+        "            <descriptorRef>jar-with-dependencies</descriptorRef>" + nl +
+        "          </descriptorRefs>" + nl +
+        "          <appendAssemblyId>false</appendAssemblyId>" + nl +
+        "          <attach>false</attach>" + nl +
+        "          <archive>" + nl +
+        "            <manifest>" + nl +
+        "              <mainClass>org.sample.Main</mainClass>" + nl +
+        "            </manifest>" + nl +
+        "          </archive>" + nl +
+        "        </configuration>" + nl +
+        "        <executions>" + nl +
+        "          <execution>" + nl +
+        "            <id>make-assembly</id>" + nl +
+        "            <phase>package</phase>" + nl +
+        "            <goals><goal>single</goal></goals>" + nl +
+        "          </execution>" + nl +
+        "        </executions>" + nl +
+        "      </plugin>" + nl
+      );
+  }
+}
