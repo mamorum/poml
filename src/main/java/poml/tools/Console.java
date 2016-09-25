e2b6472416e58e8b5c24d8ce16578fd371012499
mamorum <mamoru310@yahoo.com>
2016-09-25 16:57:02 +0900

    Polish console message.

--- src/main/java/poml/tools/Console.java
+++ src/main/java/poml/tools/Console.java
@@ -4,19 +4,21 @@ import java.lang.management.ManagementFactory;
 
 public class Console {
   public static void start(String srcFile) {
-    System.out.print("[POML:INFO] Processing ");
-    System.out.println(srcFile);
+    System.out.print("[POML:INFO] Processing \"");
+    System.out.print(srcFile);
+    System.out.println("\"");
   }
   public static void error(Throwable e, String dstFile) {
     System.err.print("[POML:ERROR] ");
     System.err.println(e.getMessage());
-    System.err.print("[POML:ERROR] Could not generate ");
-    System.err.println(dstFile);
+    System.err.print("[POML:ERROR] Could not generate \"");
+    System.err.print(dstFile);
+    System.err.println("\"");
   }
   public static void end(String dstFile) {
-    System.out.print("[POML:INFO] Genarated ");
+    System.out.print("[POML:INFO] Genarated \"");
     System.out.print(dstFile);
-    System.out.print(" @");
+    System.out.print("\" @");
     System.out.print(
         ManagementFactory
           .getRuntimeMXBean().getUptime());
