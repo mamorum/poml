20f254882deee2044386bfabb4820f05ead5f0c5
mamorum <mamoru310@yahoo.com>
2016-09-27 18:26:00 +0900

    Add comment to poml.cmd.

--- dist/poml.cmd
+++ dist/poml.cmd
@@ -2,11 +2,11 @@
 
 setlocal
 
-@rem -> find java
+@REM # Find java.exe
 for %%I in (java.exe) do set POML_JAVA=%%~$PATH:I
 if "%POML_JAVA%"=="" set POML_JAVA=java.exe
 
-@rem -> execute jar
+@REM # Execute jar
 echo [POML:INFO] Execute "poml.jar" using "%POML_JAVA%"
 set POML_ARG=%*
 if "%POML_ARG%"=="" (
@@ -14,3 +14,7 @@ if "%POML_ARG%"=="" (
 ) else (
   %POML_JAVA% -jar %~dp0\poml.jar %POML_ARG%
 )
+
+@REM # Not using exit (exit /b) command.
+@REM # Because ERRORLEVEL is set by java.exe.
+@REM # If java.exe is not found, Win(7) set 9009 to ERRORLEVEL.
