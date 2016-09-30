91ac838c3005e36170694aa360c0e2508e06ba9e
mamorum <mamoru310@yahoo.com>
2016-09-30 22:24:06 +0900

    Update cmd for win.

--- dist/poml.cmd
+++ dist/poml.cmd
@@ -1,20 +1,13 @@
 @echo off
-
 setlocal
 
-@REM # Find java.exe
-for %%I in (java.exe) do set POML_JAVA=%%~$PATH:I
-if "%POML_JAVA%"=="" set POML_JAVA=java.exe
-
-@REM # Execute jar
-echo [POML:INFO] Execute "poml.jar" using "%POML_JAVA%"
 set POML_ARG=%*
 if "%POML_ARG%"=="" (
-  %POML_JAVA% -jar %~dp0\poml.jar pom.poml pom.xml
+  java.exe -jar %~dp0\poml.jar pom.poml pom.xml
 ) else (
-  %POML_JAVA% -jar %~dp0\poml.jar %POML_ARG%
+  java.exe -jar %~dp0\poml.jar %POML_ARG%
 )
 
-@REM # Not using exit (exit /b) command.
-@REM # Because ERRORLEVEL is set by java.exe.
-@REM # If java.exe is not found, Win(7) set 9009 to ERRORLEVEL.
+@REM # Not using exit (exit /b) to set %errorlevel%.
+@REM # Because it is set by java.exe.
+@REM # If java.exe is not found, Win set error code to it.
