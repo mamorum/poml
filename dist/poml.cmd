f639a223e0e124abb27dbc251fe72b881c514bfb
mamorum <mamoru310@yahoo.com>
2016-09-30 23:21:38 +0900

    Update cmd for win.

--- dist/poml.cmd
+++ dist/poml.cmd
@@ -1,13 +1,7 @@
 @echo off
-setlocal
 
-set POML_ARG=%*
-if "%POML_ARG%"=="" (
+if "%*"=="" (
   java.exe -jar %~dp0\poml.jar pom.poml pom.xml
 ) else (
-  java.exe -jar %~dp0\poml.jar %POML_ARG%
+  java.exe -jar %~dp0\poml.jar %*
 )
-
-@REM # Not using exit (exit /b) to set %errorlevel%.
-@REM # Because it is set by java.exe.
-@REM # If java.exe is not found, Win set error code to it.
