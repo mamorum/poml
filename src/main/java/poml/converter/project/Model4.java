c190b6a1fd2466006436dc8427211dd29cd307d1
mamorum <mamoru310@yahoo.com>
2016-10-27 19:17:03 +0900

    Polish.

--- src/main/java/poml/converter/project/Model4.java
+++ src/main/java/poml/converter/project/Model4.java
@@ -14,7 +14,7 @@ public class Model4 {
         "xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" " +
         "xsi:schemaLocation=\"http://maven.apache.org/POM/4.0.0 " +
         "http://maven.apache.org/xsd/maven-4.0.0.xsd\">"); 
-      xml.out.println(sp2 + "<modelVersion>4.0.0</modelVersion>");
+      xml.out.println("  <modelVersion>4.0.0</modelVersion>");
     }
   }
   public static class End implements Converter {
