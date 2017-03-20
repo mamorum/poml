30911b9d4b92acea88abcb402657ab69a794f037
mamorum <mamoru310@yahoo.com>
2017-03-20 23:31:36 +0900

    Update test of property.

--- src/test/java/poml/conv/basic/PropertyTest.java
+++ src/test/java/poml/conv/basic/PropertyTest.java
@@ -35,20 +35,47 @@ public class PropertyTest extends ConvTestCase {
     );
   }
   
-  @Test public void replace() {
-    poml.conf.append("property=");
-    poml.conf.append("  property:value,");
-    poml.conf.append("  $encoding:UTF-8,");
-    poml.conf.append("  $compiler:1.8");
+  @Test public void replaceEncoding() {
+    poml.conf.append("property=$encoding:UTF-8");
     poml.conf.load();
     conv.convert(poml, xml);
     output.is(
       "  <properties>" + nl + 
-      "    <property>value</property>" + nl +
       "    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>" + nl +
       "    <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>" + nl +
+      "  </properties>" + nl
+    );
+  }
+  @Test public void replaceCompiler() {
+    poml.conf.append("property=$compiler:1.8");
+    poml.conf.load();
+    conv.convert(poml, xml);
+    output.is(
+      "  <properties>" + nl + 
       "    <maven.compiler.source>1.8</maven.compiler.source>" + nl +
       "    <maven.compiler.target>1.8</maven.compiler.target>" + nl +
+      "  </properties>" + nl
+    );
+  }
+  
+  @Test public void replaceMulti() {
+    poml.conf.append("property=");
+    poml.conf.append("  property1:value1,");
+    poml.conf.append("  $encoding:UTF-8,");
+    poml.conf.append("  property2:value2,");
+    poml.conf.append("  $compiler:1.8,");
+    poml.conf.append("  property3:value3");
+    poml.conf.load();
+    conv.convert(poml, xml);
+    output.is(
+      "  <properties>" + nl + 
+      "    <property1>value1</property1>" + nl +
+      "    <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>" + nl +
+      "    <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>" + nl +
+      "    <property2>value2</property2>" + nl +
+      "    <maven.compiler.source>1.8</maven.compiler.source>" + nl +
+      "    <maven.compiler.target>1.8</maven.compiler.target>" + nl +
+      "    <property3>value3</property3>" + nl +
       "  </properties>" + nl
     );
   }
