8fbeb9923cb8ce198072a87ecb4ef9d592d5e1a5
mamorum <mamoru310@yahoo.com>
2017-04-09 15:52:50 +0900

    Polish plugins.

--- src/main/java/poml/conv/build/Plugins.java
+++ src/main/java/poml/conv/build/Plugins.java
@@ -37,35 +37,4 @@ public class Plugins  implements Converter {
     }
     xml.out.add("    </plugins>").nl();
   }
-  
-  static class Plugin implements Converter {
-    String name;
-    public Plugin(String name) { this.name = name; }
-    @Override public String name() { return name; }
-
-    private static final String[] tags = {
-      "groupId", "artifactId", "version", "extensions", "inherited"
-    };
-    @Override  public void convert(Poml poml, Xml xml) {
-      String val = poml.conf.val(name);
-      String[] vals = val.split(":");
-      xml.out.add("      <plugin>").nl();
-      xml.outTags(sp8, tags, vals);  // groupId - inherited
-      convert(name, ".conf", "configuration", poml, xml);
-      convert(name, ".depend", "dependencies", poml, xml);
-      convert(name, ".depend", "executions", poml, xml);
-      xml.out.add("      </plugin>").nl();
-    }
-    private void convert(
-      String keyPrefix, String keySuffix, String tag, Poml poml, Xml xml
-    ) {
-      StringBuilder sb = new StringBuilder();
-      String key = sb.append(keyPrefix).append(keySuffix).toString();
-      if (poml.conf.has(key)) {
-        xml.out.add(sp8).add("<").add(tag).add(">").nl();
-        xml.out.add(poml.conf.tag(key, sp10));
-        xml.out.add(sp8).add("</").add(tag).add(">").nl();
-      }
-    }
-  }
 }
